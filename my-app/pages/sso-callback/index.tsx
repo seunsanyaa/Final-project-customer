ba0d5ac45c500ac7d2caf4f5c2bba1e53b9ece93
ZZSavagelordZZ <khashab2004@gmail.com>
2024-10-18 21:55:19 +0300

    Update index.tsx

--- my-app/pages/sso-callback/index.tsx
+++ my-app/pages/sso-callback/index.tsx
@@ -1,5 +1,5 @@
 import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
  
 export default function SSOCallBack() {
-  return <AuthenticateWithRedirectCallback  signUpFallbackRedirectUrl={'/onboarding'} signInFallbackRedirectUrl={'/onboarding'}/>;
+  return <AuthenticateWithRedirectCallback  signUpFallbackRedirectUrl={'/vehicles'} signInFallbackRedirectUrl={'/vehicles'}/>;
 }
