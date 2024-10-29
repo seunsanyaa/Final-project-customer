e60528b5dab247f0c9802320d5888927fbfc7c65
ALLAM-GHAREBI <144265255+ALLAM-GHAREBI@users.noreply.github.com>
2024-10-29 22:50:40 +0200

    update-access1

--- my-app/components/general/accessibilty/access1.tsx
+++ my-app/components/general/accessibilty/access1.tsx
@@ -25,25 +25,19 @@ export function Access1() {
           <section className="flex justify-between items-center mb-8">
             <div className="text-left">
               <p className="text-4xl md:text-4xl lg:text-6xl font-bold">
-              Driving with Confidence, Accessible Rentals for Everyone
+              Driving with Confidence, Accessible Rentals for All
               </p>
+              <p className="text-muted-foreground mt-4">
+              Discover Renta’s accessible fleet, designed for comfort and ease. With spacious interiors and advanced features, our wheelchair-accessible vehicles make travel simple and enjoyable for everyone. Let us provide you with a seamless, comfortable journey wherever your destination awaits.              </p>
             </div>
             <img
               src="https://res.cloudinary.com/dbsxjsktb/image/upload/v1730212805/Honda_Odyssey4_odbyjm.jpg"
               alt="Honda Odyssey"
               width={100}
               height={200}
-              className="w-[900px] h-[500px] object-cover"
+              className="w-[700px] h-[500px] object-cover"
 
             />
-            {/* <img
-              src="https://res.cloudinary.com/dbsxjsktb/image/upload/v1730213376/Honda_Odyssey6_v5nbyg.webp"
-              alt="Honda Odyssey"
-              width={100}
-              height={200}
-              className="w-[900px] h-[500px] object-cover"
-
-            /> */}
           </section>
           <section className="flex justify-between items-center mb-8">
             <img
@@ -52,7 +46,6 @@ export function Access1() {
               width={100}
               height={200}
               className="w-[500px] h-[400px] object-cover"
-
             />
             <img
               src="https://res.cloudinary.com/dbsxjsktb/image/upload/v1730213363/Honda_Odyssey5_dnwhd3.jpg"
@@ -60,11 +53,11 @@ export function Access1() {
               width={100}
               height={200}
               className="w-[500px] h-[400px] object-cover"
-
             />
           </section>
           <section ref={ref3} className={`relative w-full h-auto overflow-hidden bg-gradient-to-b from-gray-200 to-white ${ 
             inView3 ? 'animate-fadeInUp' : "opacity-0"}`} > 
+            <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight leading-tight text-center">Experience Seamless Accessibility with Every Journey</h2> 
             <div className="max-w-full mx-auto h-full"> 
               <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 mt-20 pl-4 pr-4 mb-20"> 
                 <div className="flex flex-col items-center justify-center gap-4 p-6"> 
