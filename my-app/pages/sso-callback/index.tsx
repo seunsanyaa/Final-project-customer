204731826f2e1ae5cdeee7fe313b50360f814e57
seunsanyaa <55216847+seunsanyaa@users.noreply.github.com>
2024-12-05 08:22:56 +0200

    fix : REROUTE TO DASHBOARD

--- my-app/pages/sso-callback/index.tsx
+++ my-app/pages/sso-callback/index.tsx
@@ -1,31 +1,30 @@
 import { AuthenticateWithRedirectCallback, useUser } from "@clerk/nextjs";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function SSOCallback() {
-  const { user, isLoaded } = useUser();
-  const router = useRouter();
+  const { user,  } = useUser();
   const staffMember = useQuery(api.staff.getStaffByEmail, { email: user?.emailAddresses[0]?.emailAddress ?? "" });
 
-  useEffect(() => {
-    if (isLoaded) {
-      // Check if user signed up via social
-      const isSocialSignup = user?.externalAccounts?.length > 0;
 
-      if (isSocialSignup) {
-        // Redirect to onboarding for password setup
-        router.push('/onboarding');
-      } else {
-        // Redirect based on staff or regular user
-        router.push(staffMember?.email ? "/staff" : "/");
-      }
-    }
-  }, [isLoaded, user, staffMember, router]);
+  // WE DONT NEED THIS BECAUSE WE ARE USING CLERK
+  // useEffect(() => {
+  //   if (isLoaded) {
+  //     // Check if user signed up via social
+  //     const isSocialSignup = user?.externalAccounts?.length > 0;
+
+  //     if (isSocialSignup) {
+  //       // Redirect to onboarding for password setup
+  //       router.push('/onboarding');
+  //     } else {
+  //       // Redirect based on staff or regular user
+  //       router.push(staffMember?.email ? "/staff" : "/");
+  //     }
+  //   }
+  // }, [isLoaded, user, staffMember, router]);
 
   return <AuthenticateWithRedirectCallback 
-            afterSignInUrl={staffMember?.email ? "/staff" : "/"} 
+            afterSignInUrl={staffMember?.email ? "https://car-rental-fullstack.vercel.app" : "/"} 
             afterSignUpUrl="/onboarding" 
          />;
 }
