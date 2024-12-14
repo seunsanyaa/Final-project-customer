a7699d705a14a43a824ba6cffebcd2f6f959b82d
seunsanyaa <55216847+seunsanyaa@users.noreply.github.com>
2024-12-14 04:21:11 +0200

    added: verify token for admin

--- my-app/convex/_generated/api.d.ts
+++ my-app/convex/_generated/api.d.ts
@@ -29,6 +29,7 @@ import type * as settings from "../settings.js";
 import type * as staff from "../staff.js";
 import type * as users from "../users.js";
 import type * as util from "../util.js";
+import type * as verify from "../verify.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -54,6 +55,7 @@ declare const fullApi: ApiFromModules<{
   staff: typeof staff;
   users: typeof users;
   util: typeof util;
+  verify: typeof verify;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
