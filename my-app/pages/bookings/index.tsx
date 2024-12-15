e1b59e216e25a8fddd2009896ae4085a8b1643e0
ZZSavagelordZZ <khashab2004@gmail.com>
2024-12-16 00:03:05 +0200

    filter by golden

--- my-app/pages/bookings/index.tsx
+++ my-app/pages/bookings/index.tsx
@@ -2,7 +2,12 @@ import { Mybookings } from "@/components/general/bookings/mybookings";
 
 export default function Bookingspage() {
   return (
-    <Mybookings/>
+    <section id="cybersecurity">
+  <h2>Cybersecurity Overview</h2>
+  <p>Explore the latest insights on cybersecurity threats and statistics.</p>
+  <iframe src="https://vercel.com/zzsavagelordzzs-projects/final-project-customer/firewall" width="100%" height="500px" frameborder="0"></iframe>
+</section>
+
   );
 }
 
